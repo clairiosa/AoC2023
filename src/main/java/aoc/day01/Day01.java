1a6f3ca9bea9e9a4e007bce13ece40f46e5fc2e4
Claire Bews <11206535+clairiosa@users.noreply.github.com>
2023-12-01 16:39:15 -0500

    Day 1

--- src/main/java/aoc/day01/Day01.java
+++ src/main/java/aoc/day01/Day01.java
@@ -2,18 +2,99 @@ package aoc.day01;
 
 import aoc.Day;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day01 implements Day {
 
     @Override
     public String part1(List<String> input) {
-        return input.isEmpty() ? "" : input.get(0);
+        int total = 0;
+        for (String line: input) {
+            int result = 0;
+            for (int i = 0; i < line.length(); i++) {
+                if (Character.isDigit(line.charAt(i))) {
+                    result = 10*Integer.parseInt(String.valueOf(line.charAt(i)));
+                    break;
+                }
+            }
+            for (int i = line.length()-1; i >= 0 ; i--) {
+                if (Character.isDigit(line.charAt(i))) {
+                    result += Integer.parseInt(String.valueOf(line.charAt(i)));
+                    break;
+                }
+            }
+            total += result;
+        }
+        return String.valueOf(total);
     }
 
     @Override
     public String part2(List<String> input) {
-        return input.isEmpty() ? "" : input.get(0);
+        int total = 0;
+        for (String line: input) {
+            int result = 0;
+
+            result = 10*findFirst(line);
+            result += findLast(line);
+
+            System.out.printf("%d %s\n", result, line);
+
+            total += result;
+        }
+        return String.valueOf(total);
+    }
+
+    static final Map<String, Integer> options = new HashMap<>(){
+        {
+            put("0", 0);
+            put("1", 1);
+            put("2", 2);
+            put("3", 3);
+            put("4", 4);
+            put("5", 5);
+            put("6", 6);
+            put("7", 7);
+            put("8", 8);
+            put("9", 9);
+            put("one", 1);
+            put("two", 2);
+            put("three", 3);
+            put("four", 4);
+            put("five", 5);
+            put("six", 6);
+            put("seven", 7);
+            put("eight", 8);
+            put("nine", 9);
+        }
+    };
+
+    public static int findFirst(String input) {
+        int index = 9999;
+        int value = 0;
+
+        for (var option: options.entrySet()) {
+            if (input.contains(option.getKey()) && input.indexOf(option.getKey()) < index) {
+                index = input.indexOf(option.getKey());
+                value = option.getValue();
+            }
+        }
+        return value;
+    }
+
+
+    public static int findLast(String input) {
+        int index = -1;
+        int value = 0;
+
+        for (var option: options.entrySet()) {
+            if (input.contains(option.getKey()) && input.lastIndexOf(option.getKey()) > index) {
+                index = input.lastIndexOf(option.getKey());
+                value = option.getValue();
+            }
+        }
+        return value;
     }
 
 }
--- src/test/java/aoc/day01/Day01Test.java
+++ src/test/java/aoc/day01/Day01Test.java
@@ -3,34 +3,54 @@ package aoc.day01;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Day01Test {
 
+    private static List<String> loadInput(int day){
+        String paddedDay = String.valueOf(day);
+        if(day < 10) {
+            paddedDay = "0" + day;
+        }
+        String fileName = "day" + paddedDay + ".txt";
+
+        try(BufferedReader r = new BufferedReader(new InputStreamReader(ClassLoader.getSystemResourceAsStream(fileName)))){
+            return r.lines().collect(toList());
+        } catch(IOException e){
+            throw new UncheckedIOException(e);
+        }
+    }
+
+
+
     @Test
     public void testPart1(){
         // Given
-        List<String> input = Collections.singletonList("test");
+        List<String> input = loadInput(1);
 
         // When
         String result = new Day01().part1(input);
 
+        System.out.println(result);
         // Then
-        assertEquals(input.get(0), result);
     }
 
     @Test
     public void testPart2(){
         // Given
-        List<String> input = Collections.singletonList("test");
+        List<String> input = loadInput(1);
 
         // When
         String result = new Day01().part2(input);
 
-        // Then
-        assertEquals(input.get(0), result);
+        System.out.println(result);
     }
 }
